e0bcef226be1fe4b9d56ee53847632d1d2fb609b
“rvnrngl” <0217.raven.ringel@gmail.com>
2024-07-16 16:38:08 +0800

    added dependency chakra ui library

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
+import { Auth } from "./pages/Auth/Auth";
+
 function App() {
   return (
     <>
-      <div className="flex h-screen w-screen items-center justify-center bg-teal-300 text-3xl">
-        Hello HRIS
-      </div>
+      <Auth />
     </>
   );
 }
---
+++ src/pages/Auth/Auth.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Auth = () => {
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      Auth
+    </div>
+  );
+};
